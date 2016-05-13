e23d18363bf337d0bdea78d31e680bc74517ece2
juanditoro <ditorojuanf@gmail.com>
2016-05-13 18:19:58 -0400

    Fix CommerWeek Script

--- assets/js/cw_script.js
+++ assets/js/cw_script.js
@@ -1,15 +1,19 @@
+var mq = window.matchMedia( "(max-width: 500px)" );
 $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
-
+  var multiplyWindowsH =  $(window).height() * 1.1;
   $(".cw_header_container img"). css({
     'transform' : 'translate(0px, '+wScroll/2+ '%)'
   });
-
-  if (wScroll > $('.cw_content_2_icc').offset().top - ($(window).height() / 1.05)){
+  if (mq.matches) {
+    multiplyWindowsH = $(window).height() * 2.1;
+  }
+  if (wScroll > $('.cw_content_2_icc').offset().top - multiplyWindowsH){
     $('.cw_content_2_icc').addClass('cw_show_zoomin');
     if($('.cw_content_2_icc').hasClass('cw_show_zoomin')){
-      setTimeout(function(){$('.cw_quote').addClass('cw_show_zoomin');    
+      setTimeout(function(){$('.cw_quote').addClass('cw_show_zoomin');
     },1000);
     }
+
 }
 });
